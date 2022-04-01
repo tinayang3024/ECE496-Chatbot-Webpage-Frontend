7786da182d7abeeeda99aa5f1e57b8265b8cf142
Dingyu Yang <tinayang3024@gmail.com>
2022-04-01 13:47:38 -0400

    index page changes

--- chatbot-webpage/src/PersonalityPage.js
+++ chatbot-webpage/src/PersonalityPage.js
@@ -4,47 +4,57 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import UofT from './uoft_logo.png';
+// import UofT from './uoft_logo.png';
+import SendIcon from '@mui/icons-material/Send';
+// import { styled } from '@mui/material/styles';
+import styled, { css } from 'styled-components';
+import Switch from '@mui/material/Switch';
+import chatbot_logo from './chatbot.png';
 import {
     BrowserRouter as Router,
-    Switch,
-    Route,
     Link
   } from "react-router-dom";
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 function PersonalityPage() {
-    const [personality, setPersonality] = React.useState('');
+    const [formal, setFormal] = React.useState(false);
+    const [label, setLabel] = React.useState("Formal Chatbot");
     const handleChange = (event) => {
-        console.log("personality: " + (event.target.value))
-        setPersonality(event.target.value);
+        setFormal(!formal);
+        if (formal) {
+            setLabel("Formal Chatbot")
+        } else {
+            setLabel("Informal Chatbot")
+        }
     };
     return (
         <div className="personality-page">
             <div className="logo">
-                <img src={UofT} alt="UofT" />
+                <img src={chatbot_logo} alt="Chatbot Icon" />
             </div>
             <br />
             <br />
             <div className="inputs">
-                <Box
-                    component="form"
+                
+                <FormControlLabel
+                    id="control-label"
                     sx={{
-                        '& > :not(style)': { m: 1, width: '25ch' },
+                    display: 'block',
                     }}
-                    noValidate
-                    autoComplete="off"
-                    >
-                    <TextField 
-                        id="user-personality" 
-                        label="What is your personality?" 
-                        variant="standard" 
-                        value={personality}
-                        onChange={handleChange}/>
-                </Box>
+                    control={
+                        <Switch
+                        checked={formal}
+                        onChange={handleChange}
+                        name="style"
+                        color="primary"
+                    />
+                    }
+                    label={label}
+                />
                 <br/>
                 <Link to="/chat-page">
                     <Stack spacing={2} direction="row">
-                        <Button variant="outlined">Talk with our AI Chatbot</Button>
+                        <Button id="redirect-button" variant="outlined" endIcon={<SendIcon />}>Talk with our Paraphrased Chatbot</Button>
                     </Stack>
                 </Link>
             </div>
